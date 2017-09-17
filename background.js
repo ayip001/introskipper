a53975db5793a982d2911398f1f7627198e0698b
collin yan <collinyan1996@gmail.com>
2017-09-17 00:33:31 -0700

    alpha v 0.0.1 not working

--- background.js
+++ background.js
@@ -1,3 +1,19 @@
+function getVideoID(url) {
+  var patt = new RegExp(
+    "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:" +
+    "[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+  );
+  if (patt.test(url)&&url.indexOf("t=")==-1){
+    return true;
+  }
+}
 chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
-  val url
+  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    function(tabs){
+       myurl = tabs[0].url+"&feature=youtu.be&t=05s";
+       if(getVideoID(tabs[0].url)){}
+        chrome.tabs.update(undefined, {url: myurl});
+      }
+    }
+ );
 });
